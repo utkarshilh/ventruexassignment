b6ad9643f9bb055bf96cb482c4dc72eb2995cf62
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2024-05-01 02:43:29 +0530

    implemented cards

--- src/Components/Testymonials/Testymonials.js
+++ src/Components/Testymonials/Testymonials.js
@@ -1,29 +1,39 @@
-import React from 'react'
+import React from 'react';
 import Card from './Card';
 
-export default function Testymonials() {
+export default function Testimonials() {
   return (
-    <div className="container mx-auto p-4">
-
-      <table align='center'>
-        <tr>
-          <td style={{ marginRight: '20px' }}>
-            <Card title="Protduct Name" rating={4} description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit turpis et eros luctus, vel dignissim justo accumsan." />
-          </td>
-          <td>
-            <Card title="Product Name" rating={4} description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit turpis et eros luctus, vel dignissim justo accumsan." />
-          </td>
-        </tr>
-        <tr>
-          <td style={{ marginRight: '20px' }}>
-            <Card title="Product Name" rating={4} description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit turpis et eros luctus, vel dignissim justo accumsan." />
-          </td>
-          <td>
-            <Card title="Product Name" rating={4} description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit turpis et eros luctus, vel dignissim justo accumsan." />
-          </td>
-        </tr>
-      </table>
-
+    <div className="container mx-auto p-12">
+      <div className="grid grid-cols-2 gap-2">
+        <div>
+          <Card
+            title="Product Name"
+            rating={4}
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit turpis et eros luctus, vel dignissim justo accumsan."
+          />
+        </div>
+        <div>
+          <Card
+            title="Product Name"
+            rating={4}
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit turpis et eros luctus, vel dignissim justo accumsan."
+          />
+        </div>
+        <div>
+          <Card
+            title="Product Name"
+            rating={4}
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit turpis et eros luctus, vel dignissim justo accumsan."
+          />
+        </div>
+        <div>
+          <Card
+            title="Product Name"
+            rating={4}
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit turpis et eros luctus, vel dignissim justo accumsan."
+          />
+        </div>
+      </div>
     </div>
   );
 }
