d585bd7b3f3c740ae67c1d19873f492ba1df4c5b
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2024-05-01 01:43:53 +0530

    Created all the components

---
+++ src/Components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <h1>This is home section</h1>
+        </div>
+    )
+}
+
---
+++ src/Components/Testymonials.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Testymonials() {
+    return (
+        <div>
+            <h1>This is Testymonials section</h1>
+        </div>
+    )
+}
