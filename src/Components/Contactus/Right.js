5ce5fd8b7f2955c447d805ede6084f70830c816e
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2024-05-01 15:12:04 +0530

    no need of this componet now but for the safety uploading

---
+++ src/Components/Contactus/Right.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function Right() {
+    return (
+        <div>
+            <div className="container mx-auto" >
+
+                <input
+                    className="rounded-full border border-red-900 px-4 py-2 my-1  bg-orange-300"
+                    type="text"
+
+                />
+                <br />
+                <input
+                    className="rounded-full border border-red-900 px-4 py-2 my-1 bg-orange-300"
+                    type="text"
+
+                />
+                <br />
+                <input
+                    className="rounded-full border border-red-900 px-4 py-2 my-1  bg-orange-300"
+                    type="text"
+
+                />
+                <br />
+                <button
+                    className="border border-red-900 px-4 py-2 rounded-lg"
+                    type="button"
+                ></button>
+            </div>
+        </div>
+    )
+}
