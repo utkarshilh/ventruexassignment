71fbb0aaa5d982780105508d0f87b97fa8caec23
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2024-05-01 14:27:58 +0530

    now working fine

--- src/Components/Testymonials/Testymonials.js
+++ src/Components/Testymonials/Testymonials.js
@@ -1,37 +1,55 @@
 import React from 'react';
 import Card from './Card';
+import Button from '../../Tools/Button';
+import Heading from '../../Tools/Heading';
 
 export default function Testimonials() {
   return (
-    <div className="container mx-auto p-12">
-      <div className="grid grid-cols-2 gap-2">
-        <div>
-          <Card
-            title="Product Name"
-            rating={4}
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit turpis et eros luctus, vel dignissim justo accumsan."
-          />
+    <div style={{ backgroundColor: 'rgb(254,243,199)' }}>
+      <div className="container mx-auto p-12" >
+        <div className="text-center">
+          <Heading name="TESTYMONIALS" />
         </div>
-        <div>
-          <Card
-            title="Product Name"
-            rating={4}
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit turpis et eros luctus, vel dignissim justo accumsan."
-          />
+        <div className="grid grid-cols-2 gap-2">
+          <div>
+            <Card
+              title="Product Name"
+              rating={4}
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit turpis et eros luctus, vel dignissim justo accumsan."
+            />
+          </div>
+          <div>
+            <Card
+              title="Product Name"
+              rating={4}
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit turpis et eros luctus, vel dignissim justo accumsan."
+            />
+          </div>
+          <div>
+            <Card
+              title="Product Name"
+              rating={4}
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit turpis et eros luctus, vel dignissim justo accumsan."
+            />
+          </div>
+          <div>
+            <Card
+              title="Product Name"
+              rating={4}
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit turpis et eros luctus, vel dignissim justo accumsan."
+
+            />
+          </div>
         </div>
-        <div>
-          <Card
-            title="Product Name"
-            rating={4}
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit turpis et eros luctus, vel dignissim justo accumsan."
-          />
-        </div>
-        <div>
-          <Card
-            title="Product Name"
-            rating={4}
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit turpis et eros luctus, vel dignissim justo accumsan."
-          />
+        <div className="flex justify-center mt-8">
+          {/* <button
+          className="bg-yellow-400 text-yellow-900 font-semibold py-2 px-6 border border-gray-400 rounded-full shadow"
+          style={{ width: '150px', height: '40px' }}
+        >
+          hello
+        </button> */}
+
+          <Button name="Help" />
         </div>
       </div>
     </div>
