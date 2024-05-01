9d0921a34d4874226a3552ea67beaaa67e6839e5
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2024-05-01 21:09:20 +0530

    updated heading

--- src/Tools/Heading.js
+++ src/Tools/Heading.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 export default function Heading(props) {
     return (
-
-        <h1 className=' text-5xl font-bold text-red-950 mb-8 '>{props.name}</h1>
+        <div>
+            <h1 className=' text-5xl font-bold text-red-950 mb-8 '>{props.name}</h1>
+        </div>
     )
 }
