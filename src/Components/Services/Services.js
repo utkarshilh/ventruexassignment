cae387ed4f1336da36ef704199aa67df0a499b7a
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2024-05-01 21:10:42 +0530

    created service working on it at this point it is working

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import Button from '../../Tools/Button'
+
 
 export default function Services() {
     return (
-        <div>
-            <h1>This is Services section</h1>
+        <div className='bg-amber-900'>
+            <div className='flext flex justify-center'>
+                <h1 className=' text-5xl font-bold text-white mb-8 '>SERVICES</h1>
+            </div>
+            <div className="flex justify-center">
+                <div class="grid grid-cols-4 gap-4">
+                    <div className="bg-white p-4 w-40 h-40 text-center rounded-lg">01</div>
+                    <div className="bg-white p-4 w-40 h-40 rounded-lg">02</div>
+                    <div className="bg-white p-4 w-40 h-40 rounded-lg">03</div>
+                    <div className="bg-white p-4 w-40 h-40 rounded-lg">04</div>
+                    <div className="bg-white p-4 w-40 h-40 rounded-lg">05</div>
+                    <div className="bg-white p-4 w-40 h-40 rounded-lg">06</div>
+                    <div className="bg-white p-4 w-40 h-40 rounded-lg">07</div>
+                    <div className="bg-white p-4 w-40 h-40 rounded-lg">08</div>
+                </div>
+            </div>
+            <div className='flex justify-center mt-8'>
+                <Button name="Help" />
+            </div>
         </div>
     )
 }
