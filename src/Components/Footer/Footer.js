9c8173dcf29eafca5314fa960b6c36fd8c75b5b1
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2024-05-01 21:09:55 +0530

    created footer later teh color will be changed

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className="bg-red-950 h-14 w-full">
+            {/* You can add content here if needed */}
+        </div>
+    )
+}
