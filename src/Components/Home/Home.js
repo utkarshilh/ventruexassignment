fd2040bcb71d4bbfe4efc7d98c23398187952500
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2024-05-02 02:11:42 +0530

    everything is working fine

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react';
+import Button from '../Tools/Button';
+import HomePhoto from '/Users/apple/Desktop/new projects/tailWindCSs/venturexassignment/src/Photo/HomePhoto.png';
 
 export default function Home() {
-    return (
-        <div>
-            <h1>This is home section</h1>
-        </div>
-    )
-}
+    const [headingWidth, setHeadingWidth] = useState(0);
+    const headingRef = useRef(null);
+    const subheadingRef = useRef(null);
+    const paragraphRef = useRef(null);
 
+    useEffect(() => {
+        setHeadingWidth(headingRef.current.offsetWidth);
+    }, []);
+
+    useEffect(() => {
+        if (headingWidth > 0) {
+            subheadingRef.current.style.letterSpacing = `${headingWidth / 16}px`; // Assuming font-size is 4xl (16px)
+            paragraphRef.current.style.width = `${headingWidth}px`;
+        }
+    }, [headingWidth]);
+
+    return (
+        <div style={{ backgroundColor: 'rgb(254,243,199)' }}>
+            <div className="flex flex-col md:flex-row items-center justify-center gap-8 mx-auto max-w-4xl">
+                {/* Left Part */}
+                <div className="flex flex-col items-start flex-grow">
+                    <h1 ref={headingRef} className="text-8xl font-bold mb-2 text-red-950">What the</h1>
+                    <h2 ref={subheadingRef} className='text-4xl font-bold mb-2 text-red-950'>Bear Bears</h2>
+                    <p ref={paragraphRef} className="text-lg text-red-950">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, maiores natus?</p>
+                    <div className="flex gap-4 mt-2">
+                        <Button name="help" />
+                        <a href="" className='mt-2 text-red-950'>AboutUs</a>
+                    </div>
+                </div>
+
+                {/* Right Part */}
+                <div>
+                    <img src={HomePhoto} alt="Image" className="w-full h-auto md:w-80 md:h-auto " />
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/Components/Testymonials/Testymonials.js
+++ src/Components/Testymonials/Testymonials.js
@@ -8,7 +8,7 @@ export default function Testimonials() {
     <div style={{ backgroundColor: 'rgb(254,243,199)' }}>
       <div className="container mx-auto p-12" >
         <div className="text-center">
-          <Heading name="TESTYMONIALS" gbcolor="text-red-950" />
+          <Heading name="TESTYMONIALS" />
         </div>
         <div className="grid grid-cols-2 gap-2">
           <div>
