d26f85cd0a0eea63f5ee9a1712aa18234479ef50
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2024-05-02 00:21:20 +0530

    button and heading having issue

--- src/Tools/Heading.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Heading(props) {
-    return (
-        <div>
-            <h1 className=' text-5xl font-bold text-red-950 mb-8 '>{props.name}</h1>
-        </div>
-    )
-}
